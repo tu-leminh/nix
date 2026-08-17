3ca7b6c8721b41095d3a8fcddf0adfe7dcc5a239
mt <mt.the.dev@gmail.com>
2026-08-18 00:40:51 +0700

    Trim Noctalia SysMon bar metrics

--- modules/home/noctalia.nix
+++ modules/home/noctalia.nix
@@ -40,14 +40,7 @@ in
         center = [ "clock" ];
         end = [
           "sysmon_cpu"
-          "sysmon_cpu_temp"
-          "sysmon_gpu_temp"
-          "sysmon_gpu_usage"
-          "sysmon_gpu_vram"
-          "sysmon_ram_used"
           "sysmon_ram_pct"
-          "sysmon_swap_pct"
-          "sysmon_disk_pct"
           "sysmon_net_rx"
           "sysmon_net_tx"
           "tray"
@@ -66,10 +59,8 @@ in
       system.monitor = {
         enabled = true;
         cpu_poll_seconds = 2.0;
-        gpu_poll_seconds = 5.0;
         memory_poll_seconds = 2.0;
         network_poll_seconds = 3.0;
-        disk_poll_seconds = 10.0;
       };
 
       dock.shadow = false;
@@ -99,59 +90,9 @@ in
           show_label = true;
           label_show_units = true;
         };
-        sysmon_cpu_temp = {
-          type = "sysmon";
-          stat = "cpu_temp";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
-        sysmon_gpu_temp = {
-          type = "sysmon";
-          stat = "gpu_temp";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
-        sysmon_gpu_usage = {
-          type = "sysmon";
-          stat = "gpu_usage";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
-        sysmon_gpu_vram = {
-          type = "sysmon";
-          stat = "gpu_vram";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
-        sysmon_ram_used = {
-          type = "sysmon";
-          stat = "ram_used";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
         sysmon_ram_pct = {
           type = "sysmon";
           stat = "ram_pct";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
-        sysmon_swap_pct = {
-          type = "sysmon";
-          stat = "swap_pct";
-          display = "text";
-          show_label = true;
-          label_show_units = true;
-        };
-        sysmon_disk_pct = {
-          type = "sysmon";
-          stat = "disk_pct";
-          path = "/";
           display = "text";
           show_label = true;
           label_show_units = true;
